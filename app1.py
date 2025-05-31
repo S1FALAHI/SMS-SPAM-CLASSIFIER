e70b977b73b17a69a6beb1f91993053a60dad8e9
S1FALAHI <falahiyaseen@gmail.com>
2025-05-31 18:21:41 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -50,9 +50,11 @@ st.sidebar.info(
     **Email/SMS Spam Classifier**
 
     This is a web application built using Machine learning technology that detects whether a message is **Spam** or **Not Spam**.
+    
     **Model Evaluation** :
     Accuracy  : 98.3%
     Precision : 100%
+    
     **Algorithm used** : Naive Bayes especially the Bernoulli Naive Bayes with TF-IDF Vectorization.
     
     **Project by :** Syed Yaseen (24466-CM-095) 
